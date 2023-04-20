e1b3726c1b538608785d9bbf8424142e57b3ccc9
Paulo Lacerda <paulo@findme.id>
2023-04-20 19:00:42 -0300

    feature: implements multer and import csv

---
+++ src/modules/cars/useCases/ importCategory/importCategoryUseCase.ts
@@ -0,0 +1,61 @@
+/* eslint-disable no-console */
+import fs from 'fs';
+import { parse } from 'csv-parse';
+import { ICategoriesRepository } from '../../../../@types/ICategoriesRepository';
+
+interface IIMportCategory {
+  name: string;
+  description: string;
+}
+
+class ImportCategoryUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IIMportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const categories: IIMportCategory[] = [];
+      const stream = fs.createReadStream(file.path);
+
+      const parseFile = parse({});
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line;
+          categories.push({
+            name,
+            description,
+          });
+        })
+        .on('end', () => {
+          fs.promises.unlink(file.path);
+          resolve(categories);
+        })
+        .on('error', (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.map((category) => {
+      const { name, description } = category;
+
+      console.log(name, description);
+
+      const existCategory = this.categoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description,
+        });
+      }
+    });
+  }
+}
+
+export { ImportCategoryUseCase };
